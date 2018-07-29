7d385130785597bfd50a176c447a9478b072c0be
Habib <hbrawnak@gmail.com>
2018-07-29 23:14:12 +0600

    settings updated

--- weather/views.py
+++ weather/views.py
@@ -8,15 +8,15 @@ def index(request):
     key = '806fc11050d77b8f86428810573a0b2f'
     url = 'http://api.openweathermap.org/data/2.5/weather?q={}&units=imperial&appid=' + key
 
-    city = 'Chittagong'
+    city = 'Tongi'
 
-    r  = requests.get(url.format(city)).json()
+    r = requests.get(url.format(city)).json()
 
     city_weather = {
         'city': city,
         'temperature': r['main']['temp'],
         'description': r['weather'][0]['description'],
-        'icon': r['weather'][0]['icon'],
+        'icon': r['weather'][0]['icon']
     }
 
     context = {'city_weather': city_weather}
